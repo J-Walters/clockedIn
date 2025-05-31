f5d05d6a025ac4656c6a32cd17b13ee8859ae3b6
Jordan Walters <jwalters012@gmail.com>
2025-05-30 20:58:09 -0400

    style(notification): intial styling

--- background.js
+++ background.js
@@ -2,10 +2,11 @@ const STORAGE_KEY = 'inputKey';
 
 async function checkAlarmState() {
   const result = await chrome.storage.local.get(STORAGE_KEY);
-  const isEnabled = result[STORAGE_KEY];
+  const alarmEnabled = result[STORAGE_KEY];
 
-  if (isEnabled) {
+  if (alarmEnabled) {
     const alarm = await chrome.alarms.get(STORAGE_KEY);
+
     if (!alarm) {
       await chrome.alarms.create(STORAGE_KEY, { periodInMinutes: 1 });
     }
@@ -17,8 +18,9 @@ checkAlarmState();
 chrome.alarms.onAlarm.addListener((alarm) => {
   chrome.notifications.create({
     type: 'basic',
+    iconUrl: 'icon.png',
     title: 'Time to check for jobs!',
-    message: 'Don’t forget to apply to jobs today!',
+    message: 'Don’t forget to run your ClockedIn search today!',
     silent: false,
   });
 });
