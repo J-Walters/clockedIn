da673f339416888b83793bf6cfa76ec89931ec7f
Jordan Walters <jwalters012@gmail.com>
2025-05-31 18:00:29 -0400

    chore: temporarily comment out background.js

--- background.js
+++ background.js
@@ -1,32 +1,32 @@
-/*global chrome*/
-const STORAGE_KEY = 'inputKey';
+// /*global chrome*/
+// const STORAGE_KEY = 'inputKey';
 
-async function checkAlarmState() {
-  const result = await chrome.storage.local.get(STORAGE_KEY);
-  const [alarmEnabled, frequency] = result[STORAGE_KEY];
+// async function checkAlarmState() {
+//   const result = await chrome.storage.local.get(STORAGE_KEY);
+//   const [alarmEnabled, frequency] = result[STORAGE_KEY];
 
-  if (alarmEnabled) {
-    const alarm = await chrome.alarms.get(STORAGE_KEY);
+//   if (alarmEnabled) {
+//     const alarm = await chrome.alarms.get(STORAGE_KEY);
 
-    if (!alarm) {
-      await chrome.alarms.create(STORAGE_KEY, {
-        periodInMinutes: frequency,
-      });
-    }
-  }
-}
+//     if (!alarm) {
+//       await chrome.alarms.create(STORAGE_KEY, {
+//         periodInMinutes: frequency,
+//       });
+//     }
+//   }
+// }
 
-checkAlarmState();
+// checkAlarmState();
 
-chrome.alarms.onAlarm.addListener(() => {
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: '/assets/icon.png',
-    title: 'Time to check for jobs!',
-    message: 'Don’t forget to run your ClockedIn search today!',
-    silent: false,
-  });
-});
+// chrome.alarms.onAlarm.addListener(() => {
+//   chrome.notifications.create({
+//     type: 'basic',
+//     iconUrl: '/assets/icon.png',
+//     title: 'Time to check for jobs!',
+//     message: 'Don’t forget to run your ClockedIn search today!',
+//     silent: false,
+//   });
+// });
 
-chrome.runtime.onStartup.addListener(checkAlarmState);
-chrome.runtime.onInstalled.addListener(checkAlarmState);
+// chrome.runtime.onStartup.addListener(checkAlarmState);
+// chrome.runtime.onInstalled.addListener(checkAlarmState);
