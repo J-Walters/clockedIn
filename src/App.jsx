0fde454ed1539f675160dcd41ec5d12f8d425146
Jordan Walters <jwalters012@gmail.com>
2025-06-02 14:46:39 -0400

    feat: sign in with email/ password basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -14,55 +14,73 @@ export default function App() {
   );
   const [signedIn, setSignedIn] = useState(false);
 
-  const handleGoogleSignIn = async () => {
-    const manifest = chrome.runtime.getManifest();
-    const redirectUri = `https://${chrome.runtime.id}.chromiumapp.org`;
+  //google sign in
+  // const handleGoogleSignIn = async () => {
+  // const manifest = chrome.runtime.getManifest();
+  //   const redirectUri = `https://${chrome.runtime.id}.chromiumapp.org`;
 
-    const url = new URL('https://accounts.google.com/o/oauth2/auth');
-    url.searchParams.set('client_id', manifest.oauth2.client_id);
-    url.searchParams.set('response_type', 'id_token');
-    url.searchParams.set('access_type', 'offline');
-    url.searchParams.set('redirect_uri', redirectUri);
-    url.searchParams.set('scope', manifest.oauth2.scopes.join(' '));
-    // url.searchParams.set('nonce', Math.random().toString(36).substring(2));
+  //   const url = new URL('https://accounts.google.com/o/oauth2/auth');
+  //   url.searchParams.set('client_id', manifest.oauth2.client_id);
+  //   url.searchParams.set('response_type', 'id_token');
+  //   url.searchParams.set('access_type', 'offline');
+  //   url.searchParams.set('redirect_uri', redirectUri);
+  //   url.searchParams.set('scope', manifest.oauth2.scopes.join(' '));
+  //   // url.searchParams.set('nonce', Math.random().toString(36).substring(2));
 
-    chrome.identity.launchWebAuthFlow(
-      {
-        url: url.href,
-        interactive: true,
-      },
-      async (redirectedTo) => {
-        if (chrome.runtime.lastError || !redirectedTo) {
-          console.error('OAuth failed:', chrome.runtime.lastError);
-          return;
-        }
+  //   chrome.identity.launchWebAuthFlow(
+  //     {
+  //       url: url.href,
+  //       interactive: true,
+  //     },
+  //     async (redirectedTo) => {
+  //       if (chrome.runtime.lastError || !redirectedTo) {
+  //         console.error('OAuth failed:', chrome.runtime.lastError);
+  //         return;
+  //       }
 
-        try {
-          const finalUrl = new URL(redirectedTo);
-          const params = new URLSearchParams(finalUrl.hash.substring(1));
-          const idToken = params.get('id_token');
+  //       try {
+  //         const finalUrl = new URL(redirectedTo);
+  //         const params = new URLSearchParams(finalUrl.hash.substring(1));
+  //         const idToken = params.get('id_token');
 
-          if (!idToken) {
-            throw new Error('ID token not found');
-          }
+  //         if (!idToken) {
+  //           throw new Error('ID token not found');
+  //         }
 
-          const { data, error } = await supabase.auth.signInWithIdToken({
-            provider: 'google',
-            token: idToken,
-          });
+  //         const { data, error } = await supabase.auth.signInWithIdToken({
+  //           provider: 'google',
+  //           token: idToken,
+  //         });
 
-          if (error) {
-            console.error('Supabase login error:', error);
-          } else {
-            console.log('✅ Logged in user:', data.user);
+  //         if (error) {
+  //           console.error('Supabase login error:', error);
+  //         } else {
+  //           console.log('✅ Logged in user:', data.user);
 
-            setSignedIn(true);
-          }
-        } catch (err) {
-          console.error('Sign-in flow error:', err);
-        }
+  //           setSignedIn(true);
+  //         }
+  //       } catch (err) {
+  //         console.error('Sign-in flow error:', err);
+  //       }
+  //     }
+  //   );
+  // };
+
+  const signIn = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: 'valid.email@supabase.io',
+        password: 'example-password',
+      });
+
+      console.log('data', data);
+
+      if (error) {
+        throw error;
       }
-    );
+    } catch (error) {
+      console.error('Sign In Error', error);
+    }
   };
 
   useEffect(() => {
@@ -75,13 +93,16 @@ export default function App() {
   return (
     <div className='container'>
       <header>
-        {signedIn ? (
+        <button onClick={signIn}>SIGN IN BUTTON</button>
+        {/* {signedIn ? (
           <>
             <p>Hello, Jordan</p>
           </>
         ) : (
-          <button onClick={handleGoogleSignIn}>Sign In</button>
-        )}
+          <button onClick={handleGoogleSignIn} className='secondary-button'>
+            Sign In
+          </button>
+        )} */}
         <h1>ClockedIn</h1>
       </header>
       <nav className='tabs'>
